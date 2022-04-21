69d1d869f274f5aa983927836891b427f675af1a
aj2967 <aj2967@hotmail.com>
2022-04-21 12:09:17 +0100

    Refactor portfolio section code

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,13 @@
 import { Modal } from 'bootstrap';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ReactGa from 'react-ga';
 import './portfolio.css';
-import P1 from '../../assets/p1.jpg'
-import P2 from '../../assets/p2.png'
-import P3 from '../../assets/p3.png'
-import pokedexImg from '../../assets/pokedex-img.jpg';
-import pacman from '../../assets/pacman.png'
+import automotiveInnovationsCover from '../../assets/p1.jpg'
+import lakefrontLodgingCover from '../../assets/p2.png'
+import puffinCover from '../../assets/p3.png'
+import pokedexCover from '../../assets/pokedex-img.jpg';
+import pacmanCover from '../../assets/pacman.png'
+import planets3dCover from '../../assets/planets3dCover.png'
 
 const Portfolio = () => {
 
@@ -21,23 +21,47 @@ const Portfolio = () => {
   const portfolioData = [
     {
       id: 1,
-      image: P1,
-      title: 'Automotive Innovations',
-      link: 'https://github.com/aj2967/front-end-assignment',
-      demo: 'https://automotive-innovations.netlify.app/index.html',
-      modalId: 'examplemodal-automotive-innovations',
-      modaltitle: ' Automotive Innovations',
-      modalText: 'text'
+      image: lakefrontLodgingCover,
+      title: 'Lakefront Lodging',
+      link: '',
+      demo: 'http://lakefrontlodging.epizy.com/',
+      buttonId: 'lakefront-lodging',
+      modalId: 'lakefront-lodging',
+      modaltitle: 'A hotel reservation website to allow guests to book rooms',
+      modalText: 'Users can sign in and book selected rooms with the information stored into the database for all guests.'
     },
     {
       id: 2,
-      image: P2,
-      title: 'Lakefront Lodging',
-      link: 'http://guthub.com',
-      demo: 'http://lakefrontlodging.epizy.com/',
-      modalId: 'examplemodal-lakefront-lodging',
-      modaltitle: ' Lakefront Lodging',
-      modalText: 'text'
+      image: automotiveInnovationsCover,
+      title: 'Automotive Innovations',
+      link: 'https://github.com/aj2967/front-end-assignment',
+      demo: 'https://automotive-innovations.netlify.app/index.html',
+      buttonId: 'automotive-innovations',
+      modalId: 'automotive-innovations',
+      modaltitle: 'A website on presenting the latest news on car innovations',
+      modalText: 'A responsive front-end developed website that serves as a platform to congregate and deliver engaging news and articles.'
+    },
+    {
+      id: 3,
+      image: pokedexCover,
+      title: 'Pokedex',
+      link: '',
+      demo: 'https://aj2967.github.io/pokemon/',
+      buttonId: 'pokedex',
+      modalId: 'pokedex',
+      modaltitle: 'An index of over 800 pokemons to view from',
+      modalText: `A small project created using React Js and PokeAPI. Users are able to view all pokemons and view the pokemon's individual stats.`
+    },
+    {
+      id: 4,
+      image: pacmanCover,
+      title: 'Game Database',
+      link: '',
+      demo: 'https://aj2967.github.io/games-database/',
+      buttonId: 'game-database',
+      modalId: 'game-database',
+      modaltitle: 'Details on a huge collection of games',
+      modalText: `Users curious about getting details for a game are able to search using the data provided by RAWG's API and find a variety of information or filter their search for finding something close to their needs.`
     },
   ]
 
@@ -47,50 +71,23 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio-container">
+        {/* Portfolio Map */}
+        {portfolioData.map(portfolio => (
+          <article className='portfolio-item' key={portfolio.id}>
+          <div className="portfolio-item-image">
+            <img src={portfolio.image} alt='Img' />
+          </div>
+          <h3>{portfolio.title}</h3>
+          <div className="portfolio-item-cta">
+            <button type="button" className="btn" data-bs-toggle="modal" data-bs-target={`#${portfolio.buttonId}`}>More Info</button>
+            <a href={portfolio.demo} className='btn btn-primary' target='_blank' onClick={handleViews}>Live Demo</a>
+          </div>
+        </article>
+        )).reverse()}
 
         <article className='portfolio-item'>
           <div className="portfolio-item-image">
-            <img src={pacman} alt='Img' />
-          </div>
-          <h3>Game Database</h3>
-          <div className="portfolio-item-cta">
-            <button type="button" className="btn" data-bs-toggle="modal" data-bs-target='#modal-game'>More Info</button>
-            <a href='https://aj2967.github.io/games-database/' className='btn btn-primary' target='_blank' onClick={handleViews}>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio-item'>
-          <div className="portfolio-item-image">
-            <img src={pokedexImg} alt='Img' />
-          </div>
-          <h3>Pokedex</h3>
-          <div className="portfolio-item-cta">
-            <button type="button" className="btn" data-bs-toggle="modal" data-bs-target='#modal-pokedex'>More Info</button>
-            <a href='https://aj2967.github.io/pokemon/' className='btn btn-primary' target='_blank' onClick={handleViews}>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio-item'>
-          <div className="portfolio-item-image">
-            <img src={P1} alt='Img' />
-          </div>
-          <h3>Automotive Innovations</h3>
-          <div className="portfolio-item-cta">
-            <button type="button" className="btn" data-bs-toggle="modal" data-bs-target='#modal-automotive-innovations'>More Info</button>
-            <a href='https://automotive-innovations.netlify.app/index.html' className='btn btn-primary' target='_blank' onClick={handleViews}>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio-item'>
-          <div className="portfolio-item-image">
-            <img src={P2} alt='Img' />
-          </div>
-          <h3>Lakefront Lodging</h3>
-          <div className="portfolio-item-cta">
-            <button type="button" className="btn" data-bs-toggle="modal" data-bs-target='#modal-lakefront-lodging'>More Info</button>
-            <a href='http://lakefrontlodging.epizy.com/' className='btn btn-primary' target='_blank' onClick={handleViews}>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio-item'>
-          <div className="portfolio-item-image">
-            <img src={P3} alt='Img' />
+            <img src={puffinCover} alt='Img' />
           </div>
           <h3>Puffin Group</h3>
           <div className="portfolio-item-cta">
@@ -99,65 +96,22 @@ const Portfolio = () => {
         </article>
       </div>
 
-      {/* Modal Game*/}
-        <div className="modal fade" id="modal-game" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      {/* Modal Map*/}
+        {portfolioData.map(portfolio => (
+          <div className="modal fade" id={portfolio.modalId} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" key={portfolio.id}>
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Details on a huge collection of games</h5>
+                <h5 className="modal-title" id="exampleModalLabel">{portfolio.modaltitle}</h5>
                 <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
-                Users curious about getting details for a game are able to search using the data provided by RAWG's API and find a variety of information or filter their search for finding something close to their needs.
+                {portfolio.modalText}
               </div>
             </div>
           </div>
         </div>
-      
-      {/* Modal 1*/}
-        <div className="modal fade" id="modal-automotive-innovations" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">A website on presenting the latest news on car innovations</h5>
-                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                A responsive front-end developed website that serves as a platform to congregate and deliver engaging news and articles.
-              </div>
-            </div>
-          </div>
-        </div>
-      
-      {/* Modal 2*/}
-        <div className="modal fade" id="modal-lakefront-lodging" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">A hotel reservation website to allow guests to book rooms</h5>
-                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                Users can sign in and book selected rooms with the information stored into the database for all guests.
-              </div>
-            </div>
-          </div>
-        </div>
-      
-      {/* Modal 3*/}
-        <div className="modal fade" id="modal-pokedex" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">An index of over 800 pokemons to view from</h5>
-                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                A small project created using React Js and PokeAPI. Users are able to view all pokemons and view the pokemon's individual stats.
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
     </section>
   )
 }
